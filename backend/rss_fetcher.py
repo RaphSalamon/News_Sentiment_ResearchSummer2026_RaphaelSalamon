bf29d5968b175f70b09e1babeb44831e15379948
Raphael Salamon <137216688+RaphSalamon@users.noreply.github.com>
2026-05-26 17:17:08 -0400

    implemented multi source with ticker filter for rss fetcher

--- backend/rss_fetcher.py
+++ backend/rss_fetcher.py
@@ -1,18 +1,30 @@
 import feedparser
 
 def search_ticker( ticker):
-    url=  f'https://finance.yahoo.com/rss/headline?s={ticker}' #the url which the rss feed pull from
-    feed=feedparser.parse(url)
-    headlines=[]
+    urls=  [f'https://finance.yahoo.com/rss/headline?s={ticker}', #yahoo_url
+            'https://cnbc.com/id/100003114/device/rss/rss.html',#cnbc_url
+            'https://feeds.reuters.com/reuters/businessNews',# reuters url
+            'https://feeds.marketwatch.com/marketwatch/topstories',#marketwatch url
+            'https://www.prnewswire.com/rss/news-releases-list.rss',#PRNEWSWIRE url
+            'https://feeds.apnews.com/apnews/business',#associated_press_url
+            ]
+    
+    headlines=[] #result array
 
-    for entry in feed.entries:
-        headlines.append({
-            'title': entry.title,
-            'link': entry.link,
-            'published': entry.get('published','N/A')
-        })
+    ticker_lower=ticker.lower() #each url is lowercased
+    for url in urls: # loop thru each url
+        feed=feedparser.parse(url) # parsing each url 
+        for entry in feed.entries: #loops through each entry, adds title, link and published date 
+            title=entry.title.lower() # lowercase each title
+            summary=entry.get('summary','').lower() # get a summary
+            if ticker_lower in title or ticker_lower in summary: # if we see the ticker in the summary or title, we add it
+                headlines.append({
+                    'title': entry.title,
+                    'link': entry.link,
+                    'published': entry.get('published','N/A')
+                })
 
-    return headlines
+    return headlines # return the headlines
 
 
 
