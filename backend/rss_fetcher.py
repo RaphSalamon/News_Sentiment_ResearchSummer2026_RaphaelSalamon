10510026991cbd0dbed23d1ca5dccbba9507a67c
Raphael Salamon <137216688+RaphSalamon@users.noreply.github.com>
2026-05-27 01:07:08 -0400

    in process of handling edge cases for search_ticker

--- backend/rss_fetcher.py
+++ backend/rss_fetcher.py
@@ -1,6 +1,7 @@
 import feedparser
+import re
 
-def search_ticker( ticker):
+def search_ticker( ticker,company_name):
     urls=  [f'https://finance.yahoo.com/rss/headline?s={ticker}', #yahoo_url
             'https://cnbc.com/id/100003114/device/rss/rss.html',#cnbc_url
             'https://feeds.reuters.com/reuters/businessNews',# reuters url
@@ -11,25 +12,34 @@ def search_ticker( ticker):
     
     headlines=[] #result array
 
-    ticker_lower=ticker.lower() #each url is lowercased
+    ticker_lower=ticker.lower() # ticker is lowercased
+    company_name_lower=company_name.lower() #company_name
     for url in urls: # loop thru each url
-        feed=feedparser.parse(url) # parsing each url 
-        for entry in feed.entries: #loops through each entry, adds title, link and published date 
-            title=entry.title.lower() # lowercase each title
-            summary=entry.get('summary','').lower() # get a summary
-            if ticker_lower in title or ticker_lower in summary: # if we see the ticker in the summary or title, we add it
-                headlines.append({
-                    'title': entry.title,
-                    'link': entry.link,
-                    'published': entry.get('published','N/A')
-                })
+        try:
+            feed=feedparser.parse(url) # parsing each url 
+            for entry in feed.entries: #loops through each entry, adds title, link and published date 
+                title=entry.title.lower() # lowercase each title
+                summary=entry.get('summary','').lower() # get a summary
+                print("CHECKING:", title)
+                print("SUMMARY:", summary[:100])
+                print("---")
+                if re.search(r'\wo')
+                print(ticker_lower in title, company_name_lower in title, ticker_lower in summary, company_name_lower in summary)
+                if (ticker_lower in title or company_name_lower in title) or (ticker_lower in summary or company_name_lower in summary): # if we see the ticker/company name in the summary or title, we add it
+                    headlines.append({
+                        'title': entry.title,
+                        'link': entry.link,
+                        'published': entry.get('published','N/A')
+                    })
+        except:
+            continue
 
     return headlines # return the headlines
 
 
 
 if __name__ == '__main__':
-    results = search_ticker('AAPL')
+    results = search_ticker('NKE','Nike')
     for headline in results:
         print(headline['title'])
         print(headline['link'])
