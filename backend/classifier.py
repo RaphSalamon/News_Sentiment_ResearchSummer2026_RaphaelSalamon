0250cae7006882a624111e540b29dbfcde2940e7
Raphael Salamon <137216688+RaphSalamon@users.noreply.github.com>
2026-06-10 17:12:37 -0400

    classifier- analyze_headlines with numpy avg and buy/hold/avoid logic

---
+++ backend/classifier.py
@@ -0,0 +1,39 @@
+import numpy as np
+from preprocessor import preprocess_text
+from sentiment_model import finbert_model
+
+
+
+def classify_sentiment(score): # takes the sentiment score as a input
+    if score >0.5:
+        return "BUY" # BUY WHEN SCORE IS ABOVE 0.5
+    elif(score<-0.5):
+        return "AVOID" # AVOID IF ITS BELOW -0.5
+    else:
+        return " HOLD" # HOLD IF ITS IN BETWEEN
+    
+def analyze_headlines(headlines):
+    scores=[] # scores list
+    for headline in headlines:
+        scores.append(finbert_model(preprocess_text(headline))) # loop thru each headline, preprocess the text and store it in the model
+    return np.mean(scores) # return the average of scores
+
+
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    test_headlines = [
+    "Apple stock hits record high on strong earnings",
+    "Apple faces lawsuit over misleading AI claims",
+    "Apple reports strong iPhone sales in Europe"
+]
+score = analyze_headlines(test_headlines)
+label = classify_sentiment(score)
+print("Score:", score)
+print("Verdict:", label)   
